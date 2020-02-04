8277037c23edbc16701bb5020d240dcb7a8f8c57
Owen-Benner <owen.p.benner@gmail.com>
2020-02-04 16:45:25 -0600

    LogReader.cs

---
+++ Assets/XMaze_Assets/Scripts/LogReader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogReader : MonoBehaviour
+{
+
+    public struct Frame
+    {
+        float pose;
+        float time;
+        float x;
+        float y;
+    }
+
+    public struct Selection
+    {
+        int reward;
+        int score;
+    }
+
+    public struct Segment
+    {
+        float pose;
+        float time;
+        float x;
+        float y;
+        String segment;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
