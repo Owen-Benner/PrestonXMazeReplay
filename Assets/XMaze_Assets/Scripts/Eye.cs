a8c5ad2fe0c81aab2a8ca5620be18e67358c1001
Owen-Benner <owen.p.benner@gmail.com>
2020-02-25 15:25:51 -0600

    built

---
+++ Assets/XMaze_Assets/Scripts/Eye.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour
+{
+    public float xPos;
+    public float yPos;
+    public float diameter;
+
+    private Transform pupil;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pupil = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
